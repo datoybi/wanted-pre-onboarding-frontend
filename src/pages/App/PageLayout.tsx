79a49a77208eb991ad3cb23276901fad1d759d7a
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-08-10 10:08:26 +0900

    Fix: Style 수정
    mui 삭제 및 emotion-normalize 추가, emotion babel 세팅

--- src/pages/App/PageLayout.tsx
+++ src/pages/App/PageLayout.tsx
@@ -1,5 +1,16 @@
+import { css } from '@emotion/react';
+import Container from 'components/Container';
 import { ReactNode } from 'react';
 
 export function PageLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return (
+    <div
+      css={css`
+        /* background: '#f5f5f5'; */
+        background: red;
+      `}
+    >
+      <div>{children}</div>
+    </div>
+  );
 }

db7720fb18dd1800e9e2d406f0758d8f4f5e87a9
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-08-10 23:26:26 +0900

    Feat: Header Navigation 구현

--- src/pages/App/PageLayout.tsx
+++ src/pages/App/PageLayout.tsx
@@ -1,15 +1,43 @@
+import { Link } from 'react-router-dom';
 import { css } from '@emotion/react';
 import Container from 'components/UI/Container';
 import { ReactNode } from 'react';
+import { Header } from 'components/UI';
 
 export function PageLayout({ children }: { children: ReactNode }) {
+  const jwtToken = localStorage.getItem('jwt-token');
+
+  const handleLogout = () => {
+    if (jwtToken) {
+      localStorage.removeItem('jwt-token');
+    }
+  };
+
   return (
-    <div
-      css={css`
-        background: '#f5f5f5';
-      `}
-    >
+    <>
+      <Header>
+        <nav
+          css={css`
+            height: 60px;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+            width: 300px;
+          `}
+        >
+          {jwtToken ? (
+            <Link to="/signin" onClick={handleLogout}>
+              로그아웃
+            </Link>
+          ) : (
+            <>
+              <Link to="/signin">로그인</Link>
+              <Link to="/signup">회원가입</Link>
+            </>
+          )}
+        </nav>
+      </Header>
       <Container>{children}</Container>
-    </div>
+    </>
   );
 }
